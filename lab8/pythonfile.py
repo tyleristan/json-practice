426d70678465877db3dfcbea31c3d2e32d760db9
Tyler Tan <tylertan1794@gmail.com>
2024-04-11 08:23:42 +0000

    final lab8

--- lab8/pythonfile.py
+++ lab8/pythonfile.py
@@ -1,14 +1,17 @@
 import json
+import csv
 
 with open('/workspace/json-practice/data/schacon.repos.json', 'r') as file:
     data = json.load(file)
+with open('chacon.csv', mode='w') as csvfile:
+    writer = csv.writer(csvfile, delimiter=",")
+    for i in data[:5]:
+        name = i['name']
+        html = i['html_url']
+        update = i['updated_at']
+        visibility = i['visibility']
+        line = [name,html,update,visibility]
+        writer.writerow(line)
 
 
-
-for i in data:
-    name = i['name']
-    html = i['html_url']
-    update = i['updated_at']
-    visibility = i['visibility']
-    line = name,html,update,visibility
-    print(line.partition(','))
+    
